180090f7a4f08a70d92c0fc396bc274717f8f782
Aleksandr-dl <alkeksandr.seleznev@mail.ru>
2020-06-02 20:23:39 +0400

    ыф
    ва

---
+++ src/pages/main/index.js
@@ -0,0 +1,36 @@
+// первое
+
+// let name, sName, age;
+
+// name = prompt("введите ваше имя: ",);
+// sName = prompt("введите вашу фамилию: ",);
+// age = +prompt("введите ваш вазраст: ", 18);
+
+// console.log(name);
+// console.log(sName);
+// console.log(age);
+
+// let user = {};
+
+// user.name = name;
+// user.sName = sName;
+// user.age = age;
+
+// console.log(user);
+
+
+// второе
+
+
+ let age;
+ age = prompt("введите ваш вазраст: ", 18);
+
+  for (let i=1 ; i <= age; i++)
+       {
+ if      ( i % 4 !== 0){
+   console.log(i)
+  }
+  
+ }
+ 
+
