28d79028a637ef8f3f38e2ceea45880d355a7661
Aleksandr-dl <alkeksandr.seleznev@mail.ru>
2020-06-05 09:07:34 +0400

    д.з 2.3

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -22,15 +22,31 @@
 // второе
 
 
- let age;
- age = prompt("введите ваш вазраст: ", 18);
+//  let age;
+//  age = prompt("введите ваш вазраст: ", 18);
 
-  for (let i=1 ; i <= age; i++)
-       {
- if      ( i % 4 !== 0){
-   console.log(i)
-  }
+//   for (let i=1 ; i <= age; i++)
+//        {
+//   if      ( i % 4 !== 0){
+//     console.log(i)
+//    }
   
- }
- 
+//         }
+function myfunk(){
+  let useNum5 = +prompt("введите число: ", 18);
+    let timerId = setInterval (function() {
+      
+        for(var i=0; i<1; i++) {
+          
+            useNum5 +=3;
+            console.log(useNum5);
+        }
+    }, 3000)
+
+    setTimeout (function () {
+        clearInterval (timerId);
+        console.log ("clearInterval");
+    }, 9000)
+    }
+    myfunk();
 
