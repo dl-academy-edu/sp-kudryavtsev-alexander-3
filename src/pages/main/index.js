0d977ab1bc6277bc877aa6ff6833e406ca9decd9
Aleksandr-dl <alkeksandr.seleznev@mail.ru>
2020-07-22 00:19:17 +0400

    слайдер

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,52 +1,17 @@
-// первое
 
-// let name, sName, age;
-
-// name = prompt("введите ваше имя: ",);
-// sName = prompt("введите вашу фамилию: ",);
-// age = +prompt("введите ваш вазраст: ", 18);
-
-// console.log(name);
-// console.log(sName);
-// console.log(age);
-
-// let user = {};
-
-// user.name = name;
-// user.sName = sName;
-// user.age = age;
-
-// console.log(user);
+const backToTop = document.querySelector('.back-to-top_js')
 
 
-// второе
-
-
-//  let age;
-//  age = prompt("введите ваш вазраст: ", 18);
-
-//   for (let i=1 ; i <= age; i++)
-//        {
-//   if      ( i % 4 !== 0){
-//     console.log(i)
-//    }
-  
-//         }
-function myfunk(){
-  let useNum5 = +prompt("введите число: ", 18);
-    let timerId = setInterval (function() {
-      
-        for(var i=0; i<1; i++) {
-          
-            useNum5 +=3;
-            console.log(useNum5);
-        }
-    }, 3000)
-
-    setTimeout (function () {
-        clearInterval (timerId);
-        console.log ("clearInterval");
-    }, 9000)
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 900) {
+      backToTop.classList.add('back-to-top--active');
+    } else {
+      backToTop.classList.remove('back-to-top--active');
     }
-    myfunk();
+  })
 
+  
+backToTop.addEventListener('click', function () {
+    window.scrollTo(0, 0);
+
+})
